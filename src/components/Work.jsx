5c537c4a31b744a45e55f572d45b8a3887968db0
Daniel Tolani <mumytosin2016@gmail.com>
2023-03-29 18:22:08 +0100

    removed unused imports and added image alt

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,7 +11,7 @@ const Work = () => {
                 <p>I developed this site myself to showcase my work. A larger portfolio is coming soon with more projects. </p>
             </section>
             <section className="footer-section">
-                <p><a href="mailto:tolanidaniel02@gmail.com" style={{ color: '#2E3840', textDecoration: 'underline' }}>Contact me</a><img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWI1NTlmY2E4ODk2MDcxNWVhNjg3ZGI4YTA5ZTQ3MWEwYWQyZTQ0MCZjdD1z/L3gknvclvOxoP1jWlm/giphy.gif"/>if you have any questions or would like to discuss potential collaboration opportunities.</p>
+                <p><a href="mailto:tolanidaniel02@gmail.com" style={{ color: '#2E3840', textDecoration: 'underline' }}>Contact me</a><img alt="heart-gif" src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWI1NTlmY2E4ODk2MDcxNWVhNjg3ZGI4YTA5ZTQ3MWEwYWQyZTQ0MCZjdD1z/L3gknvclvOxoP1jWlm/giphy.gif"/>if you have any questions or would like to discuss potential collaboration opportunities.</p>
             </section>
         </main>
     )
