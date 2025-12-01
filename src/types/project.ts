a4e367348bd8cb8ba69c71006caf0aedfd9fe248
danieltolani <mumytosin2016@gmail.com>
2025-12-01 22:01:15 +0100

    Update project data and styles

--- src/types/project.ts
+++ src/types/project.ts
@@ -12,6 +12,8 @@ export interface SoundDesign {
   title: string;
   description: string;
   "audio-caption"?: string;
+  "channel-image"?: string;
+  "main-image"?: string;
 }
 
 export interface Credit {
@@ -26,12 +28,30 @@ export interface StyleframesBreakdown {
     content: string[];
   };
   "grid-media"?: ProjectImage[];
+  "side-images"?: ProjectImage[];
+  "layout-images"?: ProjectImage[];
+}
+
+export interface MotionProcessImages {
+  "set1"?: ProjectImage[];
+  "banner"?: string;
+  "set2"?: ProjectImage[];
+}
+
+export interface ProjectMetadata {
+  year: string;
+  role: string;
+  tools: string;
+  type: string;
 }
 
 export interface ProjectData {
   project: string;
   title: string;
   description1: string;
+  videoId?: string;
+  videoTitle?: string;
+  metadata?: ProjectMetadata;
   styleframes: ProjectImage[];
   "styleframes-title"?: string;
   "styleframes-breakdown"?: StyleframesBreakdown;
@@ -44,6 +64,7 @@ export interface ProjectData {
   "motion-process-title"?: string;
   "motion-process-par"?: string;
   "motion-process-images"?: ProjectImage[];
+  "motion-process-images-sets"?: MotionProcessImages;
   "motion-process-text-blocks"?: ProjectTextBlock[];
   "motion-text-block-1"?: string;
   "motion-text-block-2"?: string;
@@ -51,6 +72,6 @@ export interface ProjectData {
   "motion-text-block-4"?: string;
   "credits-gifs"?: ProjectImage[];
   "sound-design"?: SoundDesign;
+  "preview-media"?: ProjectImage;
   credits?: Credit[];
 }
-
